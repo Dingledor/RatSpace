da2631acd8e3b88bf2fa875a5219b24dd8e7a877
Dingledor <calvintgoodall@gmail.com>
2026-03-02 13:03:15 -0600

    Finally get Dorb working

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Step 1
-import {Application, Assets, Sprite} from 'pixi.js';
+import { Application, Assets } from 'pixi.js';
 import { GifSprite } from 'pixi.js/gif';
 
 (async() => {
@@ -13,21 +13,19 @@ import { GifSprite } from 'pixi.js/gif';
   // Step 4
   document.body.appendChild(app.canvas);
 
-  const dorbgif = await Assets.load (
+  const source = await Assets.load (
     'dorb.gif',
   );
-
+  
   const dorb = new GifSprite({
-    dorbgif,
-    x: window.innerWidth / 2,
-    y: window.innerHeight / 2,
-    anchor: 0.5,
+    source
   });
+  
 
   app.stage.addChild(dorb);
-
   dorb.anchor.set(0.5);
 
+  // Move the sprite to the center of the screen.
   dorb.x = app.screen.width / 2;
   dorb.y = app.screen.height / 2;
   
